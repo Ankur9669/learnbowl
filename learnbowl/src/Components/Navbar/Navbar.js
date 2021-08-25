b2310f153eefcfdc36af7669448b0e9198ff8a81
Ankur9669 <65125998+Ankur9669@users.noreply.github.com>
2021-08-25 20:13:47 +0530

    completed the landing page

--- learnbowl/src/Components/Navbar/Navbar.js
+++ learnbowl/src/Components/Navbar/Navbar.js
@@ -1,21 +1,56 @@
 import React from 'react'
 import "./navbar.css";
-function navbar() {
+import {useState, useEffect} from "react";
+function Navbar() 
+{
+    const [width, setWidth] = useState(window.innerWidth);
+    const [isMenuIconClicked, setMenuIconClicked] = useState(false);
+    useEffect(() => 
+    {
+        window.addEventListener("resize", handleResize);
+    }, [])
+
+    function handleResize()
+    {
+        setWidth(window.innerWidth);
+    }
     return (
         <nav className = "navbar">
             <div className = "navbar-icon">
                 <h1 className = "navbar-text">LearnBowl</h1>
             </div>
             
-            <ul className = "menu-items">
-                <li className = "items">Ankur</li>
-                <li className = "items">Priya</li>
-                <li className = "items">Priya</li>
-                <li className = "items">Sanskar</li>
-                <li className = "items">Harshu</li>
-            </ul>
+            {
+                width > 768 ? 
+                <ul className = "menu-items">
+                    <li className = "items">French</li>
+                    <li className = "items">Vedic Maths</li>
+                    <li className = "items">About Us</li>
+                    <li className = "items">Courses</li>
+                    <li className = "items">Contact Us</li>
+                </ul> 
+                : 
+                <div>
+                    <img src = "/menu.svg" className = "menu-icon" onClick = {() => setMenuIconClicked(!isMenuIconClicked)}></img>
+                    {isMenuIconClicked ? 
+                    <div className = "menu-items-mobiles">
+                        <ul className = "menu-items">
+                        <li className = "items">French</li>
+                        <li className = "items">Vedic Maths</li>
+                        <li className = "items">About Us</li>
+                        <li className = "items">Courses</li>
+                        <li className = "items">Contact Us</li>
+                        </ul> 
+                    </div> 
+                    : 
+                    <></>
+                    }
+                </div>
+                
+            }
+            
         </nav>
     )
 }
 
-export default navbar
+export default Navbar
